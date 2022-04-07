3a1ceb6ce0ffb3763ddacb00559c9fec4ba71507
goblin <jackmord445@gmail.com>
2022-04-07 13:39:57 +0800

    nana

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,6 +22,7 @@ function Index() {
   return (
     <div>
       <Helmet>
+        <title>WIP Portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
@@ -32,7 +33,7 @@ function Index() {
       <div className={`${styles.Hero}`}>
         <h1 className="text-4xl font-bold tracking-wide">DUKE LYNDELL</h1>
         <h4 className="text-lg italic tracking-wider font-light text-gray-200">Full Stack Developer</h4>
-        <h6 className=" italic text-gray-400 tracking-wider">Based on PH</h6>
+        <h6 className=" italic text-gray-400 tracking-wider">Based on Mars</h6>
 
         <div className={`${styles.HeroButtonContainer}`}>
           <button>
