abc40e601a69919dd1f49d7648f920ae4544876b
goblin <jackmord445@gmail.com>
2022-04-07 16:23:56 +0800

    add footer, add tooltip, and contacts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar/NavBar';
+import { Link } from "gatsby";
 
 
 import '../styles/index.css';
@@ -17,10 +18,12 @@ import Github from '../components/Icons/Github/Github.jsx';
 import Figma from '../components/Icons/Figma/Figma.jsx';
 import ReactIcon from '../components/Icons/React/React.jsx';
 
+import ReactTooltip from 'react-tooltip';
+
 function Index() {
 
   return (
-    <div>
+    <div className={`${styles.Container}`}>
       <Helmet>
         <title>WIP Portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
@@ -52,16 +55,38 @@ function Index() {
         </div>
 
         <div className={`${styles.SectionContent}`}>
-          <NodeJS />
-          <Javascript />
-          <HTML />
-          <CSS />
-          <ReactIcon />
-          <Github />
-          <Figma />
+          <NodeJS data-tip="NodeJS" />
+          <Javascript data-tip="Javascript"/>
+          <HTML data-tip="HTML5"/>
+          <CSS data-tip="CSS3"/>
+          <ReactIcon data-tip="React"/>
+          <Github data-tip="Github/Git Cli"/>
+          <Figma data-tip="Figma"/>
         </div>
       </div>
+    
+      <footer className={`${styles.Footer}`}>
+          <h6 className="text-gray-300 mb-4">Contacts</h6>
+          <div className="text-gray-400 text-sm space-y-2 underline flex flex-col">
 
+            <div className={`${styles.FooterContactItem}`}>
+              <Github height={25} width={25}/>
+              <a href="https://github.com/floooptimism" rel="noopener noreferrer" target="_blank">@floooptimism</a>
+    
+            </div>
+
+            <div className={`${styles.FooterContactItem}`}>
+              <svg width="24" height="23" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.25 0H1.58333C0.7125 0 0.00791666 0.7125 0.00791666 1.58333L0 11.0833C0 11.9542 0.7125 12.6667 1.58333 12.6667H14.25C15.1208 12.6667 15.8333 11.9542 15.8333 11.0833V1.58333C15.8333 0.7125 15.1208 0 14.25 0ZM14.25 3.16667L7.91667 7.125L1.58333 3.16667V1.58333L7.91667 5.54167L14.25 1.58333V3.16667Z" fill="#CECECE"/>
+              </svg>
+              <a href="mailto:feline.sr15m@aleeas.com" rel="noopener noreferrer">feline.sr15m@aleeas.com</a>
+            </div>
+
+          </div>
+
+      </footer>
+
+      <ReactTooltip effect="solid" clickable/>
     </div>
   );
 }
