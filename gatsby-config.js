e870ed4f9e7b441a0a000e5a99c40680f05e6596
goblin <jackmord445@gmail.com>
2022-04-24 20:10:47 +0800

    image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,30 @@
-module.exports = {
-  siteMetadata: {
-    title: `Gatsby + Node.js API`,
-      siteUrl: `https://gatsby-template.vercel.app`,
-  },
-  plugins: [
-    'gatsby-plugin-postcss',
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
 
-  ]
+module.exports = {
+    siteMetadata: {
+        title: `Gatsby + Node.js API`,
+        siteUrl: `https://gatsby-template.vercel.app`,
+    },
+    plugins: [
+        'gatsby-plugin-postcss',
+        {
+            resolve: `gatsby-plugin-layout`,
+            options: {
+                component: require.resolve(`./src/components/Layout/Layout.jsx`),
+            },
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+            }
+        }
+
+    ]
 }
